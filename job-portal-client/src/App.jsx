d55823e494f9817398c29276dd3053d89c0d5e3d
Pratyush0026 <113766134+Pratyush0026@users.noreply.github.com>
2024-08-29 19:03:38 +0530

    pass

--- job-portal-client/src/App.jsx
+++ job-portal-client/src/App.jsx
@@ -20,31 +20,16 @@ function App() {
     <>
       <div className="App  ">
         {loading ? (
-
           <div className="flex justify-center m-60">
-            
-          
-          <PacmanLoader
-
-              
-            
-            color={"rgb(134 25 143)"}
-            loading={loading}
-            size={70}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-
-</div>
+            <PacmanLoader
+              color={"rgb(134 25 143)"}
+              loading={loading}
+              size={70}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
         ) : (
-          // <GridLoader
-          //   color={'#D0021B'}
-          //   loading={loading}
-
-          //   size={100}
-          //   aria-label="Loading Spinner"
-          //   data-testid="loader"
-          // />
           <div>
             <Navbar />
             <Outlet />
--- job-portal-client/src/components/Login.jsx
+++ job-portal-client/src/components/Login.jsx
@@ -21,31 +21,82 @@ const Login = () => {
       });
   };
 
-  const buttonVariants = {
-    hover: {
-      scale: 1.1,
-      textShadow: "0px 0px 8px rgb(255,255,255)",
-      boxShadow: "0px 0px 8px rgb(255,255,255)",
-      rotate: 360,
-      transition: {
-        duration: 0.3,
-        yoyo: Infinity
-      },
-    },
-  };
+
+
+
+
 
   return (
-    <motion.div className='h-screen w-full flex items-center justify-center'>
-      <motion.button
-        variants={buttonVariants}
-        whileHover="hover"
-        className='bg-blue px-8 py-2 text-white'
+    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 ">
+      <h1 className="text-2xl font-bold text-secondary sm:text-3xl text-center">
+        Get Started Today
+      </h1>
+      <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, fugiat
+        dolorum laudantium totam a maxime?
+      </p>
+
+      <div className="mx-auto max-w-lg mb-0 mt-6 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+        <form className="space-y-4 ">
+          <p className="text-center text-red-400 text-lg font-medium">
+            Sign into your Account
+          </p>
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <div className="relative">
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Email"
+                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              />
+              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+            
+              </span>
+            </div>
+          </div>
+
+          {/* password */}
+          <div>
+            <label htmlFor="password" className="sr-only">
+                Password
+            </label>
+            <div className="relative">
+              <input
+                // type={ ? 'text' : 'password'}
+                name="password"
+                placeholder="Enter Password"
+                className="w-full border outline-none rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+              />
+              <span onClick={() => setShowPassword(!showPassword)} className="absolute inset-y-0 end-0 grid place-content-center px-4">
+
+               
+              </span>
+            </div>
+          </div>
+
+          <button onClick={handleLogin} type="submit" className="block w-full rounded-lg bg-secondary px-5 py-3 text-sm font-medium text-white">Sign in</button>
+          <p className="text-center text-sm text-gray-500">No account ? <button className="underline text-black" to="/register">Sign up</button></p>
+        </form>
+
+        {/* <GoogleLogin/> */}
+        <motion.button
         onClick={handleLogin}
-      >
-        Login
-      </motion.button>
-    </motion.div>
+            // variants={buttonVariants}
+            whileHover="hover"
+            type="submit"
+            className="bg-blue py-2 px-8 text-white md:rounded-s-none rounded"
+          >
+            {" "}
+            Login
+          </motion.button>
+      </div>
+    </div>
   );
+
+
 };
 
 export default Login;
